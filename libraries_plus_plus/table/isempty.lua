660de5ca878cd56a24c1002d3fa262c34330b521
zapacni <seanjruiz04@gmail.com>
2020-12-16 01:39:12 -0800

    Implement more table library functions

---
+++ libraries_plus_plus/table/isempty.lua
@@ -0,0 +1,5 @@
+local function isempty(tbl: { [any]: any }) -- idrc if it's a dict
+	return next(tbl) == nil
+end
+
+return isempty
---
+++ libraries_plus_plus/table/zip.lua
@@ -0,0 +1,19 @@
+local function zip(...) -- help
+	local args, args_values = table.pack(...), { }
+	local i = 0
+
+	return function()
+		i = i + 1
+
+		for index, tbl in ipairs(args) do
+			args_values[index] = tbl[i]
+
+			if args_values[index] == nil then
+				return nil
+			end
+		end
+		return table.unpack(args_values)
+	end
+end
+
+return zip
